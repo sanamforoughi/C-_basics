3640e94e165afb150a231a397bd16ef863204236
sanamforoughi <sanamforoughi@gmail.com>
2017-02-27 16:31:41 +0000

    Create interestratecalculator.cpp

---
+++ interestratecalculator.cpp
@@ -0,0 +1,35 @@
+//
+//  main.cpp
+//  Stock Market Simulator
+//
+//  Created by SANAM FOROUGHI on 12/28/16.
+//  Copyright © 2016 Sanam Foroughi. All rights reserved.
+//
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+
+
+int main()
+
+    //cout << " boy \n i \n love \n behnam \n I'm \n so \n screwed \n practice \n makes \n perfect "; // new line }
+    
+    //{  if (5>3) {
+     //   cout << "I need to practice calculus!"; // if statement. But how do I do one saying? If this person's age is less than 18 then give them a half fair
+   // }
+        //return 0;
+{
+    float a;
+    float p = 10000;
+    float r = .01;
+    
+    for (int day =1 ; day <=30; day ++) {
+       a = p * pow(1+r, day);
+    
+       cout << day << " " << a << endl;
+       }
+
+
+    return 0;
