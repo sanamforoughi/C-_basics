25804ab064e072c602f99901d2a2c8edc88b6710
sanamforoughi <sanamforoughi@gmail.com>
2017-12-06 00:03:35 +0000

    Update interestratecalculator.cpp

--- interestratecalculator.cpp
+++ interestratecalculator.cpp
@@ -14,12 +14,6 @@ using namespace std;
 
 int main()
 
-    //cout << " boy \n i \n love \n applied \n and \n discrete \n mathematics \n practice \n makes \n perfect "; // new line }
-    
-    //{  if (5>3) {
-     //   cout << "I need to practice calculus!"; // if statement. But how do I do one saying? If this person's age is less than 18 then give them a half fair
-   // }
-        //return 0;
 {
     float a;
     float p = 10000;
